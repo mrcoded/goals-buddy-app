8d4d2880560f90878d25613171469c963408175d
CodedLibra <adeola.ogundijo@yahoo.com>
2026-03-05 23:59:29 +0100

    feat: created a CommunityListsCard component

---
+++ components/dashboard/community-lists-card.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Link from "next/link";
+import { UserIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { UserCommunityProps } from "@/types/matches.types";
+
+const CommunityListsCard = ({
+  userCommunities,
+}: {
+  userCommunities: UserCommunityProps[] | undefined;
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <CardTitle className="flex items-center">
+            <UserIcon className="size-4 mr-2 text-primary" />
+            Communities
+          </CardTitle>
+          <Link href="/communities">
+            <Button variant="outline" size="sm">
+              Manage
+            </Button>
+          </Link>
+        </div>
+        <CardDescription>Communities you`re part of</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-3">
+          {userCommunities?.slice(0, 4)?.map((c) => {
+            return (
+              <Card className="shadow-none" key={c.community?.id}>
+                <Link href={`/communities/c/${c.community?.id}`}>
+                  <CardHeader className="text-sm">
+                    <CardTitle>{c.community?.name}</CardTitle>
+                    <CardDescription>
+                      {c.community?.description}
+                    </CardDescription>
+                  </CardHeader>
+                </Link>
+              </Card>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CommunityListsCard;
