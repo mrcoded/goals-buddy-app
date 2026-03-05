cfeae43e742d74cc7cd784f7c22f342e8fd1a42f
CodedLibra <adeola.ogundijo@yahoo.com>
2026-03-05 23:58:26 +0100

    feat: created a UserAvatar component

---
+++ components/ui/user-avatar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { cn } from "@/lib/utils";
+
+const UserAvatar = ({
+  name,
+  size,
+  imageUrl,
+}: {
+  name: string;
+  imageUrl?: string;
+  size?: "sm" | "md";
+}) => {
+  const initials = name?.[0]?.toUpperCase() || "U";
+  const originalName = name || "User";
+
+  return (
+    <Avatar
+      className={cn(
+        "size-10",
+        size === "sm" ? "size-8" : size === "md" ? "size-10" : "size-12",
+      )}
+    >
+      {imageUrl && <AvatarImage src={imageUrl} alt={originalName} />}
+      <AvatarFallback>{initials}</AvatarFallback>
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
