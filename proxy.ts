4e8f8496d62d06f891ef0bc827919ee236eac9a8
CodedLibra <adeola.ogundijo@yahoo.com>
2026-03-06 13:11:07 +0100

    feat: redirect anauthenticated users

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/api/communities/all",
+  "/auth/sign-up(.*)",
+  "/auth/signin(.*)",
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && !isPublicRoute(request)) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
