b7aae350687d25a2240b876b31fb76b61a9216e7
CodedLibra <adeola.ogundijo@yahoo.com>
2026-03-06 19:49:49 +0100

    feat: created a useGetCommunityById query and useCreateCommunity and useEditCommunity mutations hooks

--- hooks/use-communities.ts
+++ hooks/use-communities.ts
@@ -15,6 +15,24 @@ export const useCommunities = () => {
   });
 };
 
+export const useGetCommunityById = (communityId: string) => {
+  return useQuery({
+    queryKey: ["communities", communityId],
+    queryFn: async () => {
+      const res = await client.api.communities.c[":communityId"].$get({
+        param: {
+          communityId: communityId,
+        },
+      });
+      //if response fails
+      if (!res.ok) throw new Error("Failed to fetch a community");
+
+      return await res.json();
+    },
+    enabled: !!communityId,
+  });
+};
+
 export const useAllCommunities = () => {
   return useQuery({
     queryKey: ["all-communities"],
@@ -67,6 +85,61 @@ export const useJoinCommunity = () => {
     onError(error) {
       console.error("Error joining community", error);
       toast.error("Failed to join community! Try again");
+    },
+  });
+};
+
+export const useCreateCommunity = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (formData: { name: string; description: string }) => {
+      const res = await client.api.communities.c.create.$post({
+        json: {
+          name: formData.name,
+          description: formData.description,
+        },
+      });
+      //if response fails
+      if (!res.ok) throw new Error("Failed to create community");
+
+      return res.json();
+    },
+    onSuccess: () => {
+      toast.success("Successfully created community");
+      queryClient.invalidateQueries({ queryKey: ["communities"] });
+    },
+    onError(error) {
+      console.error("Error creating community", error);
+      toast.error("Failed to create community! Try again");
+    },
+  });
+};
+
+export const useEditCommunity = (communityId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (formData: { name: string; description: string }) => {
+      const res = await client.api.communities.c[":communityId"].edit.$put({
+        param: { communityId },
+        json: {
+          name: formData.name,
+          description: formData.description,
+        },
+      });
+      //if response fails
+      if (!res.ok) throw new Error("Failed to update community");
+
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["communities"] });
+      toast.success("Successfully updated community");
+    },
+    onError(error) {
+      console.error("Error updating community", error);
+      toast.error("Failed to update community! Try again");
     },
   });
 };
