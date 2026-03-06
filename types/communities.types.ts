553016fd85415229fca824d7964396b693fc8330
CodedLibra <adeola.ogundijo@yahoo.com>
2026-03-06 19:41:51 +0100

    added CommunityInputFormProps types

--- types/communities.types.ts
+++ types/communities.types.ts
@@ -7,7 +7,18 @@ export interface UserCommunitiesProps {
 
 export interface JoinedCommunityCardProps {
   showLockIcon: boolean;
+  isLoadingUserCommunities: boolean;
   userCommunities: UserCommunitiesProps[];
   selectedCommunity: string;
   setSelectedCommunity: (communityId: string) => void;
+}
+
+export interface CommunityInputFormProps {
+  mutationFn: (formData: {
+    name: string;
+    description: string;
+  }) => Promise<unknown>;
+  isPending: boolean;
+  isEditing?: boolean;
+  initialData?: { name: string; description: string };
 }
